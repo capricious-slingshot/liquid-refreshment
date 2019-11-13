fbfc5f544211ef1603f22d2637a8adca0e7a8fbd
Diapause <diapause@protonmail.com>
2019-11-12 16:04:21 -0800

    adding inital configuration

--- Gemfile
+++ Gemfile
@@ -1,13 +1,18 @@
 source "https://rubygems.org"
 
 ruby "2.6.1"
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra", "~> 2.0.7"
 gem "sequel", "~> 5.24.0"
 gem "pg", "~> 1.1.4"
 gem 'require_all', '~> 2.0'
 gem 'activerecord', '~> 5.0', '>= 5.0.0.1'
+
+
+group :development do
+
+end
 
 group :test do
   gem "rspec", "~> 3.8.0"
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,17 @@
 ENV['SINATRA_ENV'] ||= "development"
+
 require 'require_all'
 require 'bundler/setup'
 require 'active_record'
+
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require_all 'app'
-require_all 'lib'
+require './app/controllers/application_controller'
 
-ActiveRecord::Base.establish_connection("postgres://localhost/liquid_refreshment_development")
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
