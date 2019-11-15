1378f98909e136db48cadc12e8c6f88f82ab696d
Diapause <diapause@protonmail.com>
2019-11-15 14:53:51 -0800

    fixing rspec errors

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -1,47 +1,70 @@
 require 'spec_helper'
+require 'rspec'
+require 'rack/test'
 require_relative '../config/environment'
 
 describe ApplicationController do
 
   describe "Index" do
-    it 'loads the homepage' do
+
+    it 'loads the homepage and does not show My Beers navigation if not logged in' do
       get '/'
       expect(last_response.status).to eq(200)
-      expect(last_response.body).to include("Cloudburst Brewing")
-      expect(last_response.body).to include("Login")
+
+      expect(last_response.body).to include("Log In")
       expect(last_response.body).to include("Signup")
-    end
-
-    it 'shows Leaderboard / My Beers navigation only when logged in' do
-      user1 = User.create(name: "Scooby Doo", email: "ruuuf@dog.com", password: "isniffbutts")
-
-      get '/'
-      expect(last_response.status).to eq(200)
-
+      expect(last_response.body).to_not include("My Beers")
       expect(last_response.body).to include("Cloudburst Brewing")
-      expect(last_response.body).to include("My Ratings")
-      expect(last_response.body).to include("Leaderboard")
     end
   end
 
   describe "Create User" do
-    it 'Can create a new user' do
-      params = {
-        :username => "skittles123",
-        :email => "skittles@aol.com",
-        :password => "rainbows"
-      }
+    before(:each) do
+      params = {username: "skittles123", email: "skittles@aol.com", password: "rainbows"}
       get '/signup'
       expect(last_response.status).to eq(200)
       post '/signup', params
+    end
+    params = {username: "skittles123", email: "skittles@aol.com", password: "rainbows"}
+
+    it 'Can create a new user' do
       expect(last_response.location).to include("/#{params[:username]}/beers")
     end
 
     it 'cannot create a new user if logged in' do
-      get '/signup'
+      login()
       expect(last_response.body).to_not include("Log In")
       expect(last_response.location).to include("/#{params[:username]}/beers")
     end
+
+    it 'cannot create a new user with an exzisting username' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+
+      post '/signup', params
+    end
+
+    it 'cannot create a new user with a string that is not an email' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+
+      post '/signup', params
+    end
+
+    it 'cannot create a new user with an exzisting email' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+
+      post '/signup', params
+    end
+
+    it 'cannot create a new user with apassword shorter than 8 characters' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+
+      post '/signup', params
+    end
+
   end
 
 end
