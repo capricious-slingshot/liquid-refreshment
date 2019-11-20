0aed9646ce8a69d91b880b7cede7913c73fb48b1
Diapause <diapause@protonmail.com>
2019-11-20 09:58:09 -0800

    adding index page of all beers and tweaking transparancy in CSS

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,7 +1,15 @@
 class BeersController < ApplicationController
   get '/beers' do
     if logged_in?
-      erb :index
+      erb :'/beers/index', locals: { beers: Beer.all }
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/beers/:id/edit' do
+    if logged_in?
+      erb :'/beers/edit', locals: { beers: Beer.all }
     else
       redirect '/login'
     end
@@ -13,6 +21,13 @@ class BeersController < ApplicationController
       erb :'users/index', locals: {user: user}
     else
       redirect '/login'
+    end
+  end
+
+  helpers do
+    def overall_rating
+      #if rating is <= 1 "Not Enough Raitings"
+      3
     end
   end
 end
--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,6 +1,6 @@
 class Opinion < ActiveRecord::Base
-  validates :user_id, presence: true
-  validates :beer_id, presence: true
+  # validates :user_id, presence: true
+  # validates :beer_id, presence: true
   belongs_to :beers
   belongs_to :users
 end
