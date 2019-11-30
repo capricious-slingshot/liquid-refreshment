3cd1645eab68d7ad688d5e78f0691b67f1720149
Diapause <diapause@protonmail.com>
2019-11-30 14:47:19 -0800

    user can delete beer if they are owner

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -7,13 +7,27 @@ class BeersController < ApplicationController
     end
   end
 
-  get '/beers/:slug' do
-    if logged_in?
-      user = User.find_by_slug(params[:slug])
-      erb :'users/index', locals: {user: user}
-    else
-      redirect '/login'
+  get '/beers/:id' do
+    beer = Beer.find_by(id: params[:id])
+    user = User.find_by(id: session[:user_id])
+    review = user && user.opinions.find_by(beer_id: beer.id)
+    erb :'/beers/show', locals: {review: review, beer: beer}
+  end
+
+  delete '/beers/:id/delete' do
+    beer = Beer.find_by(id: params[:id])
+
+    if logged_in? && owner?(beer)
+
+      if beer.delete && beer.errors.empty?
+        flash[:success] = "Scuccessfully Deleted #{beer.name.capitalize}"
+        redirect '/beers'
+      else
+        flash[:error] = ["Something went wrong"] << user.errors.full_messages
+        redirect "/beers/#{params[:id]}"
+      end
     end
+
   end
 
   helpers do
@@ -23,6 +37,10 @@ class BeersController < ApplicationController
       else
         beer.opinions.average(:user_rating).to_f.round(1)
       end
+    end
+
+    def owner?(beer)
+      session[:user_id] == beer.created_by
     end
   end
 end
